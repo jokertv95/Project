07e024532073d96a2728c1e2a3496702b94bfc84
Algis <example@gmail.com>
2020-12-01 20:08:27 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// "use strict";
+
+// let number = 4.6;
+
+// console.log(-4 / 0);
+// console.log('string' * 9);
+
+// const persone = `Alex`;
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// // console.log(1);
+
+// var drawCats = function (howManyTimes) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//         console.log(i + " ( ˘⌣˘)♡(˘⌣˘ ) ");
+//     }
+//     };
+//     drawCats(20);
+
+// const obj = {
+//     name: "Algis",
+//     age: 25,
+//     isMarried: false,
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ["plump.png", "orange.jpg", "apple.bmp"];
+// console.log(arr[1]);
+
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer +5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Algis';
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + '-object');
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+
+console.log(2 * 4 === 8);
+
+const isCheked = true,
+    isClose = true;
+
+console.log(isCheked && isClose);
